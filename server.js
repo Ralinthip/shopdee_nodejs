f6377c6c09be1961cf83db58cc28b15a8526b5f2
Ralinthip <ralinthip.sk@gmial.com>
2024-09-17 18:54:29 +0700

    Version.1

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ const db = mysql.createConnection(
     {
         host: "localhost",
         user: "root",
-        password: "1234",
+        password: "Cs12345678",
         database: "shopdee"
     }
 )
@@ -142,6 +142,19 @@ app.post('/api/login',
     }
 );
 
+app.post('/api/admin/add',async (req,res)=>{
+    const {username, password, firstName, lastName, email, gender } = req.body;
+ 
+    const salt = await bcrypt.genSalt(10);
+    const password_hash = await bcrypt.hash(password, salt);  
+ 
+    const sql = `INSERT INTO employee(username, password, firstName, lastName, email, gender,positionID
+                )VALUES(?, ?, ?, ?, ?, ?, 1)`;            
+    db.query(sql, [username, password_hash, firstName, lastName, email, gender], (err) => {
+        if (err) throw err;
+            res.send({ 'message': 'เพิ่มข้อมูลพนักงานเรียบร้อยแล้ว', 'status': true });
+        });                    
+})
 
 // Function to execute a query with a promise-based approach
 function query(sql, params) {
@@ -312,7 +325,7 @@ app.post('/api/admin/login',
     async function(req, res){
         //Validate username
         const {username, password} = req.body;                
-        let sql = "SELECT * FROM employee WHERE username=? AND isActive = 1";        
+        let sql = "SELECT * FROM employee WHERE username=? AND isActive = 1 and positionID = 1";        
         let employee = await query(sql, [username, username]);        
         
         if(employee.length <= 0){            
@@ -468,8 +481,8 @@ app.post('/api/employee',
                     
                     //save data into database                
                     let sql = `INSERT INTO employee(
-                            username, password, firstName, lastName, email, gender
-                            )VALUES(?, ?, ?, ?, ?, ?)`;   
+                            username, password, firstName, lastName, email, gender, positionID
+                            )VALUES(?, ?, ?, ?, ?, ?, 0)`;   
                     let params = [username, password_hash, firstName, lastName, email, gender];
                 
                     db.query(sql, params, (err, result) => {
